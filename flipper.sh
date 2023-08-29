70ab2c8e9e7de8d2a0d6825ff1aac220c19b111f
Bryan Smith <bryan@bry-guy.net>
2023-08-29 02:38:39 -0400

    Unfuck the thing

--- flipper.sh
+++ flipper.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 if [ -z "$API_KEY" ]; then
 		echo "ERROR: Missing API_KEY env var."
 		exit 1
 fi
 
-SERVICE_LIST=$(curl --request GET --url https://api.render.com/v1/services --header "authorization: Bearer $API_KEY")
+echo "Finding services..."
 
-SUSPEND_TYPE_COUNT=$(echo "$SERVICE_LIST" | jq '.[].service.suspended' | grep 'srv' | awk '{print $1}' | uniq | wc -l | tr -d " ")
+SERVICE_LIST=$(curl --request GET --url https://api.render.com/v1/services --header "authorization: Bearer $API_KEY" | jq '.[].service | select(.id | startswith("srv"))')
 
-if [ "$SUSPEND_TYPE_COUNT" != "1" ]; then
+SUSPEND_STR=$(echo "$SERVICE_LIST" | jq '.suspended' | uniq | tr -d '"')
+
+SUSPEND_TYPE_COUNT=$(echo "$SERVICE_STR" | wc -l) 
+
+if [ $SUSPEND_TYPE_COUNT = "1" ]; then
+		echo "Found one suspend type."
+else
 		echo "ERROR: Too many suspend types."
 		exit 1
 fi
-
-SUSPEND_STR=$(echo "$SERVICE_LIST" | jq '.[].service.suspended' | grep 'srv' | awk '{print $1}' | uniq | tr -d '"')
 
 echo "Checking suspend string..."
 echo "Suspend String: $SUSPEND_STR"
@@ -29,13 +33,13 @@ else
 		VERB="suspend"
 fi
 
-SERVICE_IDS=$(echo "$SERVICE_LIST" | jq '.[].service.id' | grep 'srv' | tr -d '"')
+SERVICE_IDS=$(echo "$SERVICE_LIST" | jq '.id' | tr -d '"')
 
-IFS=$'\n' read -r -d '' -a lines <<< "$SERVICE_IDS"
+IFS=$'\n' read -d '' -a lines <<< "$SERVICE_IDS"
 for line in "${lines[@]}"; do
 		echo "$VERB: $line"
 		curl --request POST \
 				--url https://api.render.com/v1/services/$line/$VERB \
 				--header "accept: application/json" \
 				--header "authorization: Bearer $API_KEY"
-done
+done 
